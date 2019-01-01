f8671deae78d12620faacf93f1ca3f376c72806f
Simey de Klerk <simeydeklerk@outlook.com>
2019-01-01 11:44:10 +0200

    refactor puzzles into separate file

---
+++ src/game/boardSettables.test.ts
@@ -0,0 +1,50 @@
+import puzzles from '../puzzles/puzzles'
+import boardFromString from './boardFromString';
+import {bd} from './boardFromString.test'
+import ISettable from './ISettable';
+
+bd.updateCanBe()
+
+function settablesToString(settables: ISettable[]) {
+    return settables.map(s => `[${s.cell.row},${s.cell.column}]:${s.value}`).join(' ')
+}
+
+test('settables', () => {
+    expect(bd.cells[0].isEmpty).toBe(false)
+    expect(bd.cells[0].settable).toEqual([])
+    // console.log(settablesToString(bd.settablesFromCells))
+    // console.log(settablesToString(bd.settablesFromConstrainedSets))
+    const fromCells = `[2,2]:6`
+    const fromSets = `[0,7]:6 [1,8]:7 [2,0]:5 [2,5]:9 [5,5]:4 [8,2]:7 [8,4]:1 [2,5]:9 [5,6]:7 [2,0]:5 [2,5]:9 [1,8]:7 [5,5]:4`
+    expect(settablesToString(bd.settablesFromCells)).toEqual(fromCells)
+    expect(settablesToString(bd.settablesFromConstrainedSets)).toEqual(fromSets)
+})
+
+test('applySettables', () => {
+    bd.solve()    
+    expect(bd.isWon).toBe(true)
+})
+
+test('hard puzzle',()=> {
+    const str = puzzles.hard[0]
+
+    const b = boardFromString(str)
+    expect(b.emptyCells.length).toBe(51)
+    expect(b.isValid).toBe(true)
+    b.solve()
+    console.log(b.emptyCells.length, b.settables)
+    console.log(b.toValueString())
+    expect(b.isWon).toBe(true)
+})
+
+test('medium puzzle',()=> {
+
+    const str = puzzles.med[3]
+    const b = boardFromString(str)
+    expect(b.emptyCells.length).toBe(56)
+    expect(b.isValid).toBe(true)
+    b.solve()
+    console.log(b.emptyCells.length, b.settables)
+    console.log(b.toValueString())
+    expect(b.isWon).toBe(true)
+})
