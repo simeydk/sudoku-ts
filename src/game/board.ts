4b96d55d035eab2181207d786e665482b564ec2d
Simey de Klerk <simeydeklerk@outlook.com>
2019-01-01 11:42:09 +0200

    add solving logic

--- src/game/board.ts
+++ src/game/board.ts
@@ -10,6 +10,8 @@ class Board {
     public columns : ConstrainedSet[] = []
     public blocks : ConstrainedSet[] = []
 
+    private dirtyCells: Cell[] = []
+
     constructor() {
         constructBoard(this)
     }
@@ -44,9 +46,44 @@ class Board {
         }).join(rowDelim)
     }
     public updateCanBe() {
-        this.constrainedSets.forEach(cs => cs.updateCanBe())
-        this.completedCells.forEach(cell => cell.updateCanBe())
+        this.completedCells.forEach(cell => {
+            cell.updateCanBe()
+            cell.updateCanBeNeighbours()
+        })
     }
+
+    public applySettable(settable: ISettable) {
+        const {cell,value} = settable
+        if((this.cells.indexOf(cell) !== -1) && cell.isEmpty) {
+            cell.value = value
+            this.dirtyCells.push(cell)
+        }
+    }
+
+    public applySettablesAll() {
+        this.settables.forEach(s => this.applySettable(s))
+    }
+
+    get isComplete() {
+        return this.emptyCells.length === 0
+    }
+
+    get isValid() {
+        return this.constrainedSets.every(cs => cs.isValid)
+    }
+
+    get isWon() {
+        return this.isComplete && this.isValid
+    }
+
+    public solve() {
+        this.updateCanBe()
+        while(!this.isComplete && this.isValid && this.settables.length > 0) {
+            this.applySettablesAll()
+            this.updateCanBe()
+        }
+    }
+
 
 }
 
